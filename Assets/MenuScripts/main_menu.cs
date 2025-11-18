45f8817d9243f8ddea71bad0a8fd53f6815dd076
Darin Abankwa <Darinabankwa57@gmail.com>
2025-11-18 14:35:39 -0500

    I made the UI and got the Controls and Difficulty and quit button working the only thing that doesn't work is when you press the difficulty button and if you press easy, medium, or hard it doesn't take you anywhere yet

---
+++ Assets/MenuScripts/main_menu.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class main_menu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync("SampleScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
